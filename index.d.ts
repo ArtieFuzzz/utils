0a4610e1543a42bdba8f843958916e916460754e
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2021-12-11 22:05:48 +1100

    Better type declarations

---
+++ index.d.ts
@@ -0,0 +1,47 @@
+
+declare namespace Utilities {
+	export class StopWatch {
+		#private
+		private digits
+		constructor(digits?: number)
+		/**
+		 * Returns this StopWatch's raw duration in milliseconds
+		 */
+		get duration(): number
+		/**
+		 * Returns a boolean indicating whether or not this StopWatch is running
+		 */
+		get isRunning(): boolean
+		/**
+		 * Stops this StopWatch and returns with the human readable duration.
+		 */
+		stop(): string
+		/**
+		 * Restarts this StopWatch
+		 */
+		restart(): this
+		/**
+		 * Starts this StopWatch
+		 */
+		start(): this
+		/**
+		 * Returns with the human readable duration.
+		 */
+		private symbol
+	}
+
+	/**
+	  * Checks to see if an Object is empty or not
+	  * @params obj Value to check
+	  */
+	export function isEmptyObject(obj: Record<string, unknown>): boolean
+
+	/**
+	 * Checks if `obj` is an Object
+	 * @params obj Value to check
+	 */
+	export function isObject(obj: any): boolean
+}
+
+export = Utilities
+export as namespace Utilities
